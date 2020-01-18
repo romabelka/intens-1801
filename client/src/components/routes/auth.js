ac893207d8286bf6889747e3cfbdcf2ea965e71f
Roman Iakobchuk <kaboom.ua@gmail.com>
2020-01-18 10:49:52 +0200

    add basic auth form

--- client/src/components/routes/auth.js
+++ client/src/components/routes/auth.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import {NavLink, Route} from "react-router-dom";
+import SignUpForm from "../auth/sign-up-form";
+import SignInForm from "../auth/sign-in-form";
 
 class AuthPage extends Component {
   static propTypes = {}
@@ -7,6 +10,12 @@ class AuthPage extends Component {
     return (
       <div>
         <h1>Auth Page</h1>
+        <div>
+          <NavLink activeStyle={{color: 'red'}} to="/auth/sign-in">Sign In</NavLink>
+          <NavLink activeStyle={{color: 'red'}} to="/auth/sign-up">Sign Up</NavLink>
+        </div>
+        <Route path="/auth/sign-up" component={SignUpForm}/>
+        <Route path="/auth/sign-in" component={SignInForm}/>
       </div>
     )
   }
